a2d72c47d5e62b5adc7dccc0912789353a82547e
Ben Woodhead <bwoodhead@upei.ca>
2012-03-09 12:59:59 -0400

    Have convert to pdf and create thumbnails

--- DocumentProcessor.php
+++ DocumentProcessor.php
@@ -3,23 +3,100 @@
 
 class DocumentProcessor 
 {
-    private $pid;
     
-    
-    public function __construct($pid=null) {
+    /**
+     * Ingest a pdf 
+     */
+    public function ingestPDF($args, $dsid, $file, $file_ext) {
+        
+        // Copy the pdf 
+        $_SESSION['fedora_ingest_files']["$dsid"] = $file;
+        return TRUE;
     }
     
     /**
-     * Ingest is a hook called from the content model 
+     * Convert to pdf then ingest 
+     */
+    public function convertAndIngestPDF($args, $dsid, $file, $file_ext) {
+        
+        $returnValue = TRUE;
+        $output = array();
+
+        $destFile = dirname($file) . basename($file) . '.' . $file_suffix;
+        
+        $application = '';
+        $args ='--headless --convert-to PDF ';
+
+        if (stristr($_SERVER['SERVER_SOFTWARE'], 'microsoft')) {
+            $application = '';
+        }
+        elseif (stristr($_SERVER['SERVER_SOFTWARE'], 'linux')) {
+            $application = '';
+        }
+        elseif (stristr($_SERVER['SERVER_SOFTWARE'], 'unix')) {
+           $application = '';
+        }
+        else {
+            $application = '/Applications/LibreOffice.app/Contents/MacOS/soffice ';
+        }
+
+        // Create the file
+        system($application. " " . $args . " " . $file, $returnValue);
+
+        if ($returnValue == '0') {
+            $_SESSION['fedora_ingest_files']["$dsid"] = $destFile;
+            return TRUE;
+        }
+        else {
+            return $returnValue;
+        }        
+    }
+    
+    /**
+     * Create the thumbnail
      * @param type $args
      * @param type $dsid
      * @param type $file
      * @param type $file_ext
      * @return boolean|string 
      */
-    public function ingest($args, $dsid, $file, $file_ext) {
+    public function createThumbnail($args, $dsid, $file, $file_ext) {
+
+        // Get the width and height from the arguments
+        $height = $args['height'];
+        $width = $args['width'];
         
-        $this->createDerivatives($args, $dsid, $file, $file_ext);
+        // Get the file from the session
+        $file = $_SESSION['fedora_ingest_files']["$dsid"];
+
+        $file_suffix = '_' . $dsid . '.' . $file_ext;
+        $returnValue = TRUE;
+
+        if (stristr($_SERVER['SERVER_SOFTWARE'], 'microsoft')) {
+
+        }
+        elseif (stristr($_SERVER['SERVER_SOFTWARE'], 'linux')) {
+            $cmdline = "/usr/local/bin/convert \"$file\"\[0\] -colorspace RGB -thumbnail $width" . "x$height \"$file$file_suffix\"";
+        }
+        elseif (stristr($_SERVER['SERVER_SOFTWARE'], 'unix')) {
+            // Use this for Mac OS X (MAMP)
+            $cmdline = "sips -s format jpeg \"$file\" -z $height $height --out \"$file$file_suffix\" >/dev/null";
+        }
+        else {
+            $cmdline = "convert \"$file\"\[0\] -colorspace RGB -thumbnail " . $width . "x" . $height . " \"$file$file_suffix\"";
+        }
+
+        system($cmdline, $returnValue);
+
+        $var = $file . $file_suffix . ' returnvalue= ' . $returnValue;
+
+        if ($returnValue == '0') {
+            $_SESSION['fedora_ingest_files']["$dsid"] = $file . $file_suffix;
+            return TRUE;
+        }
+        else {
+            return $returnValue;
+        }        
     }
     
     /**
@@ -63,47 +140,7 @@ class DocumentProcessor
 
         // Render the tabset.
         return $tabset;
-
     }
-    
-    /**
-     * Create the derivatives
-     * @param type $args
-     * @param type $dsid
-     * @param type $file
-     * @param type $file_ext
-     * @return boolean|string 
-     */
-    public function createDerivatives($args, $dsid, $file, $file_ext) {
-        
-        $system = getenv('System');
-        $file_suffix = '_' . $dsid . '.' . $file_ext;
-
-        $returnValue = TRUE;
-        $output = array();
-
-        $destDir = dirname($file) . '/work';
-        $destFile = $destDir . '/' . basename($file) . $file_suffix;
-        if (!is_dir($destDir)) {
-            @mkdir($destDir);
-        }
-
-        if (!file_exists($destFile)) {
-            exec('convert -resize ' . $width . 'x' . $height . ' -quality 85  "' . $file . '"[0] -strip "' . $destFile . '" 2>&1 &', $output, $returnValue);
-        }
-        else {
-            $returnValue = '0';
-        }
-
-        if ($returnValue == '0') {
-            $_SESSION['fedora_ingest_files']["$dsid"] = $destFile;
-            return TRUE;
-        }
-        else {
-            return $returnValue;
-        }
-    }
-
 }
 
 ?>
